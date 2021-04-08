eddde5c772189af53f01d2333c9ca275385ae4d9
Vince Chow <vchow.829@gmail.com>
2021-04-07 23:38:47 -0700

    First upload of HTML, CSS, and JS file. Have layout of first questions, working on getting functionality added.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Quiz-HW
+This week seems a little better in relating the in class activities to the assignment. I am using one of the in class activities as a basis for CSS and javascript as a launching off point and changing the elements within them to make it my own.
+
---
+++ script.js
@@ -0,0 +1,95 @@
+var startButton = document.querySelector(".start-button");
+var question1El = document.querySelector(".question1");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+
+var timer;
+var timerCount;
+
+function startTimer() {
+  var timeInterval = setInterval(function () {
+  timeLeft--;
+  timerElement.textContent = timeLeft;
+  if (timeLeft === 0) {
+    clearInterval(timeInterval);
+    displayMessage ();
+  }
+  }, 1000);
+}
+
+function displayMessage() {
+  timerElement.textContent = "Your time has run out";
+}
+
+
+
+
+startButton.addEventListener("click", startTimer);
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+
+
+
+// Updates win count on screen and sets win count to client storage
+function setWins() {
+  win.textContent = winCounter;
+  localStorage.setItem("winCount", winCounter);
+}
+
+// Updates lose count on screen and sets lose count to client storage
+function setLosses() {
+  lose.textContent = loseCounter;
+  localStorage.setItem("loseCount", loseCounter);
+}
+
+// These functions are used by init
+function getWins() {
+  // Get stored value from client storage, if it exists
+  var storedWins = localStorage.getItem("winCount");
+  // If stored value doesn't exist, set counter to 0
+  if (storedWins === null) {
+    winCounter = 0;
+  } else {
+    // If a value is retrieved from client storage set the winCounter to that value
+    winCounter = storedWins;
+  }
+  //Render win count to page
+  win.textContent = winCounter;
+}
+
+// Bonus: Add reset button
+var resetButton = document.querySelector(".reset-button");
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
+  setWins()
+  setLosses()
+}
+// Attaches event listener to button
+resetButton.addEventListener("click", resetGame);
